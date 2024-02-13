3064ecc73739ddf50620017a51d777cf7d63aa0a
Monika Grzanek <grzanekmonika@o2.pl>
2024-02-13 07:56:51 +0100

    Add class AmountWidget with constructor and getElements method with reference to div with .widget-amount

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,7 @@ const select = {
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       console.log('new Product: ', thisProduct);
     }
@@ -89,6 +90,7 @@ const select = {
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
     
     initAccordion() {
@@ -178,6 +180,29 @@ const select = {
         }
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;    
+    }
+
+    initAmountWidget() {
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+    }
+  }
+
+  class AmountWidget{
+    constructor(element) {
+      const thisWidget = this;
+      thisWidget.getElements(element);
+      console.log('AmoundWidget: ', thisWidget);
+      console.log('constructor arguments: ', element);
+    }
+
+    getElements(element) {
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
     }
   }
   
