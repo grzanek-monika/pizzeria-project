ba542a5c6640962ae6e8104a2cc023de3c041228
Monika Grzanek <grzanekmonika@o2.pl>
2024-02-19 12:29:31 +0100

    Add eventListener in initAction in class Cart, which istening updated to change the number of products in the cart

--- src/js/script.js
+++ src/js/script.js
@@ -362,6 +362,9 @@
       const thisCart = this;
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       })
     }
 
