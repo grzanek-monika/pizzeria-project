92fbb7987ec9a96c0fe05276244f5d144215626d
Monika Grzanek <grzanekmonika@o2.pl>
2024-03-02 07:40:44 +0100

    Add class BaseWidget and inherit the class AmountWidget from it, add methods parseValue and isValid

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,35 +1,43 @@
 import { select, settings } from "../settings.js";
+import BaseWidget from "./BaseWidget.js";
 
-class AmountWidget{
+class AmountWidget extends BaseWidget {
     constructor(element) {
+      super(element, settings.amountWidget.defaultValue);
       const thisWidget = this;
-      thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
+      /*thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);*/
       thisWidget.initActions();
       console.log('AmoundWidget: ', thisWidget);
       console.log('constructor arguments: ', element);
     }
 
-    getElements(element) {
+    getElements() {
       const thisWidget = this;
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+      thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
 
     }
 
     setValue(value) {
       const thisWidget = this;
-      const newValue = parseInt(value);
+      const newValue = thisWidget.parseValue(value);
       /* Add validation */
-      if(thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
+      if(thisWidget.value !== newValue && thisWidget.isValid(newValue)) {
         thisWidget.value = newValue;
       } 
       thisWidget.input.value = thisWidget.value;
 
       thisWidget.announce();
      
+    }
+    parseValue(value){
+      return parseInt(value);
+    }
+
+    isValid(value){
+      return !isNaN(value) && value >= settings.amountWidget.defaultMin && value <= settings.amountWidget.defaultMax;
     }
 
     announce() {
