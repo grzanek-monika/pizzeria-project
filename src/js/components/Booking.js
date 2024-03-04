285b96117821e2ee0275b0b59e7dcc02d7706235
Monika Grzanek <grzanekmonika@o2.pl>
2024-03-04 13:54:18 +0100

    Change in pluggins and add instances of classes DatePicker and HourPicker in class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { select, templates } from "../settings.js";
 import AmountWidgets from "./AmountWidget.js"
+import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
 
 class Booking {
     constructor(element){
@@ -17,12 +19,16 @@ class Booking {
         thisBooking.dom.wrapper.innerHTML = generatedHTML;
         thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
     }
 
     initWidgets(){
         const thisBooking = this;
         thisBooking.peopleAmount = new AmountWidgets(thisBooking.dom.peopleAmount);
         thisBooking.hoursAmount = new AmountWidgets(thisBooking.dom.hoursAmount);
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
         //thisBooking.peopleAmount.addEventListener('updated', function(){});
         //thisBooking.hoursAmount.addEventListener('updated', function(){});
     }
