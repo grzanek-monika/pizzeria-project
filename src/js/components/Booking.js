7a03c9fed139dde581e4894594afa268f60f2e77
Monika Grzanek <grzanekmonika@o2.pl>
2024-03-05 08:18:52 +0100

    Add method getData in Booking to download filtered data from API, change in settings, correct in html code

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,13 +1,15 @@
-import { select, templates } from "../settings.js";
+import { select, settings, templates } from "../settings.js";
 import AmountWidgets from "./AmountWidget.js"
 import DatePicker from "./DatePicker.js";
 import HourPicker from "./HourPicker.js";
+import utils from "../utils.js";
 
 class Booking {
     constructor(element){
         const thisBooking = this;
         thisBooking.render(element);
         thisBooking.initWidgets();
+        thisBooking.getData();
     }
 
     render(element) {
@@ -32,6 +34,54 @@ class Booking {
         //thisBooking.peopleAmount.addEventListener('updated', function(){});
         //thisBooking.hoursAmount.addEventListener('updated', function(){});
     }
+
+    getData(){
+        const thisBooking = this;
+        const startStateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+        const endStateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+        const params = {
+            booking: [
+                startStateParam, endStateParam
+            ],
+            eventsCurrent: [
+                settings.db.notRepeatParam, startStateParam, endStateParam
+            ],
+            eventsRepeat: [
+                settings.db.repeatParam, endStateParam
+
+            ]
+        };
+        console.log('getData params: ', params);
+
+        const urls = {
+            booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+            eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
+            eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),
+        }
+        console.log('urls: ', urls);
+
+        Promise.all([
+            fetch(urls.booking),
+            fetch(urls.eventsCurrent),
+            fetch(urls.eventsRepeat),
+        ])
+            .then(function(allResponses){
+                const bookingsResponse = allResponses[0];
+                const eventsCurrentResponse = allResponses[1];
+                const eventsRepeatResponse = allResponses[2];
+                return Promise.all([
+                    bookingsResponse.json(),
+                    eventsCurrentResponse.json(),
+                    eventsRepeatResponse.json(),
+                ]) 
+            })
+            .then(function([bookings, eventsCurrent, eventsRepeat]){
+                console.log('bookings: ', bookings);
+                console.log('eventsCurrent: ', eventsCurrent);
+                console.log('eventsRepeat: ', eventsRepeat);
+            })
+;    }
+
 }
 
 export default Booking;
