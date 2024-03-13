7a4132dfc4709b1954568e29574201e3248d564f
Monika Grzanek <grzanekmonika@o2.pl>
2024-03-13 08:04:27 +0100

    Add html code and styles for section links in Home

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,23 +1,44 @@
+import { select, templates } from "./../settings.js";
+import utils from "../utils.js";
+
 class Home {
     constructor(element){
         const thisHome = this;
-        thisHome.render(element);
         thisHome.getElements(element);
+        thisHome.render(element);
         thisHome.initWidgets();
         thisHome.initCarousel();
         
     }
 
-
-    render(){
-
+    getElements(element){
+        const thisHome = this;
+        thisHome.dom = {};
+        thisHome.dom.wrapper = element;
+        thisHome.dom.carousel = element.querySelector(select.home.carousel);
     }
 
-    getElements(){
+    render(element){
+        const thisHome = this;
+        const generatedHTML = templates.homePage(element);
+        const generatedDOM = utils.generatedDOM(generatedHTML);
+        thisHome.dom.wrapper.appendChild(generatedDOM);
+        
 
     }
 
     initCarousel(){
+        const thisHome = this;
+        console.log(thisHome);
+        // eslint-disable-next-line no-undef
+        const options = {
+            cellAlign: 'left',
+            contain: true,
+            autoPlay: true
+          };
+
+          // eslint-disable-next-line no-undef
+          return new Flickity('.main-carousel', options);
 
     }
 }
